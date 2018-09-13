d56e41a516309d0e145bb5e1d77915b8ed950527
Slorack <ra90561@uem.br>
2018-09-13 14:35:39 -0300

    Main Danillo

---
+++ main.c
@@ -0,0 +1,12 @@
+	switch(op){
+		case 1: printf("%lf", sum(x, y));
+			break;
+		case 2: printf("%lf", sub(x, y));
+			break;
+		case 3: printf("%lf", mul(x, y));
+			break;
+		case 4:	printf("%lf", div(x, y));
+			break;
+		return 0;
+	}
+}
--- parte_1/main.c
+++ parte_1/main.c
@@ -8,4 +8,15 @@ int main() {
 	scanf("%d %d",&x,&y);
 	printf("Digite o numero da operação desejada: 1-soma\n 2-sub\n 3-mul\n 4-div\n");
 	scanf("%d",&op);
-	
+		switch(op){
+		case 1: printf("%lf", sum(x, y));
+			break;
+		case 2: printf("%lf", sub(x, y));
+			break;
+		case 3: printf("%lf", mul(x, y));
+			break;
+		case 4:	printf("%lf", div(x, y));
+			break;
+		return 0;
+	}
+}
